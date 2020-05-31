df87731466e7d94e2144acf820d0e8771a36ed66
huongnguyen <huonghuonglacey1020@gmail.com>
2020-05-31 03:33:12 -0700

    commit

--- server.js
+++ server.js
@@ -14,20 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget" , {
-//   useNewurlParser: true,
-//   useFindAndModify: false
-// ;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds243054.mlab.com:43054/heroku_5dg0v4m4"
 
-
-// mongoose.connect(MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify:false
-})
+mongoose.connect(MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify:false
+// })
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
